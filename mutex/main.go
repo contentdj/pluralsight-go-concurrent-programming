99e31fd2b27d5bcd2a5964391d357c4cae129451
ed <erdalkiran@gmail.com>
2016-11-16 04:19:17 +0300

    #000 ed prints j/1 in mutex ex

--- mutex/main.go
+++ mutex/main.go
@@ -1,19 +1,24 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	mutex := make(chan bool, 1)
 
 	for i := 1; i < 10; i++ {
 		for j := 1; j < 10; j++ {
-			mutex <- true
-			go func() {
-				fmt.Printf("%d + %d = %d\n", i, j, i+j)
-				<-mutex
-			}()
+			for z := 0; z < 2; z++ {
+				mutex <- true
+				if z%2 == 0 {
+					go func() {
+						fmt.Printf("%d + %d = %d\n", i, j, i+j)
+						<-mutex
+					}()
+				} else {
+					<-mutex
+				}
+
+			}
 		}
 	}
 
